b61ff46bc2432ed0078cc615a3b0e34048a92ae8
Nguyen Thanh An <thanhan030301@gmail.com>
2022-05-12 19:04:05 +0700

    add dotenv

--- config/DBconnect.js
+++ config/DBconnect.js
@@ -1,16 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('d84af0962luq84', 'adawzuvnwpaldy', 'fe4788a010e9d47d3596c2ca98a0d00377ebe6fc2dddc4efa1fca98e103a8903', {
-  host: 'ec2-52-4-104-184.compute-1.amazonaws.com',
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD, {
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres',
+    logging: false,
+    query: { raw: true },
+    timezone: '+07:00',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
-  }
-});
+  });
 const DBconnect = async () => {
   try {
     await sequelize.authenticate();
