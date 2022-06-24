f5a9848892758faf4b915fb2a1686389fcc7186f
Tran Xuan An <tranxuana2803@gmail.com>
2022-06-24 14:25:54 +0700

    Update-medicineReport
    Description ..........

--- config/DBconnect.js
+++ config/DBconnect.js
@@ -2,28 +2,28 @@ const { Sequelize } = require('sequelize');
 const initModels = require('../models/schemas/init-models');
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'postgres',
-    logging: false,
-    query: { raw: true },
-    timezone: '+07:00',
-    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
-  }
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USERNAME,
+    process.env.DATABASE_PASSWORD, {
+        host: process.env.DATABASE_HOST,
+        dialect: 'postgres',
+        logging: false,
+        query: { raw: true },
+        timezone: '+07:00',
+        dialectOptions: { ssl: { require: true, rejectUnauthorized: false, requestCert: false } }
+    }
 );
 
-const DBconnect = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connected');
-  } catch (error) {
-    console.error(error);
-  }
+const DBconnect = async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Database connected');
+    } catch (error) {
+        console.error(error);
+    }
 };
 module.exports = {
-  DBconnect,
-  sequelize,
-  models: initModels(sequelize)
+    DBconnect,
+    sequelize,
+    models: initModels(sequelize)
 };
